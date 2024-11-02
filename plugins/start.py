a40dc21b4911aac9a793a367847aceba895f5826
Renz <alightmotionprem229@gmail.com>
2024-11-02 16:34:40 +0700

    ee

--- plugins/start.py
+++ plugins/start.py
@@ -121,7 +121,7 @@ async def start_command(client: Bot, message: Message):
             except BaseException:
                 pass
     else:
-        out = start_button(client)
+        out = await start_button(client)
         await message.reply_text(
             text=START_MSG.format(
                 first=message.from_user.first_name,
@@ -140,7 +140,7 @@ async def start_command(client: Bot, message: Message):
 
 @Bot.on_message(filters.command("start") & filters.private)
 async def not_joined(client: Bot, message: Message):
-    buttons = fsub_button(client, message)
+    buttons = await fsub_button(client, message)
     await message.reply(
         text=FORCE_MSG.format(
             first=message.from_user.first_name,
