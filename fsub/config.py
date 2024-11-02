13e8a12b9f911ee0a14b3d71e4e3e5a92c85a888
Renz <alightmotionprem229@gmail.com>
2024-11-02 15:52:14 +0700

    Update config.py

--- fsub/config.py
+++ fsub/config.py
@@ -1,39 +1,40 @@
 import logging
-from os import getenv
+import os
+from dotenv import load_dotenv
 from distutils.util import strtobool
 from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
 
 load_dotenv("config.env")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-APP_ID = int(getenv("APP_ID"))
-API_HASH = getenv("API_HASH")
-CHANNEL_ID = int(getenv("CHANNEL_ID"))
-OWNER = getenv("OWNER")
-PROTECT_CONTENT = strtobool(getenv("PROTECT_CONTENT", "False"))
-DB_URI = getenv("DATABASE_URL")
-BUTTON_ROW = int(getenv("BUTTON_ROW", 2))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+APP_ID = int(os.getenv("APP_ID"))
+API_HASH = os.getenv("API_HASH")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+OWNER = os.getenv("OWNER")
+PROTECT_CONTENT = strtobool(os.getenv("PROTECT_CONTENT", "False"))
+DB_URI = os.getenv("DATABASE_URL")
+BUTTON_ROW = int(os.getenv("BUTTON_ROW", 2))
 FORCE_SUB_ = {}
 FSUB_TOTAL = 1
 while True:
     key = f"FORCE_SUB_{FSUB_TOTAL}"
-    value = getenv(key)
+    value = os.getenv(key)
     if value is None:
         break
     FORCE_SUB_[FSUB_TOTAL] = int(value)
     FSUB_TOTAL += 1
-START_MESSAGE = getenv(
+START_MESSAGE = os.getenv(
     "START_MESSAGE",
     "<b>🙋🏻‍♂️ʜᴀʟᴏ {mention}!</b>\n\n"
     "<b>ᴛʜᴇ ʙᴏᴛ ɪꜱ ᴜᴘ ᴀɴᴅ ʀᴜɴɴɪɴɢ. ᴛʜᴇꜱᴇ ʙᴏᴛꜱ ᴄᴀɴ ꜱᴛᴏʀᴇ ᴍᴇꜱꜱᴀɢᴇꜱ ɪɴ ᴄᴜꜱᴛᴏᴍ ᴄʜᴀᴛꜱ, ᴀɴᴅ ᴜꜱᴇʀꜱ ᴀᴄᴄᴇꜱꜱ ᴛʜᴇᴍ ᴛʜʀᴏᴜɢʜ ᴛʜᴇ ʙᴏᴛ.</b>",
 )
-FORCE_MESSAGE = getenv(
+FORCE_MESSAGE = os.getenv(
     "FORCE_MESSAGE",
     "<b>🙋🏻‍♂️ʜᴀʟᴏ {mention}!</b>\n\n"
     "<b>💬 ᴛᴏ ꜱᴇᴇ ᴛʜᴇ ᴍᴇꜱꜱᴀɢᴇꜱ ᴛʜᴇ ʙᴏᴛ ɪꜱ ꜱʜᴀʀɪɴɢ, ᴊᴏɪɴ ꜰɪʀꜱᴛ, ᴛʜᴇɴ ᴘʀᴇꜱꜱ ᴛʀʏ ᴀɢᴀɪɴ.</b>"
 )
-ADMINS = [int(x) for x in (getenv("ADMINS").split())]
+ADMINS = [int(x) for x in (os.getenv("ADMINS").split())]
 
 LOG_FILE_NAME = "logs.txt"
 logging.basicConfig(
