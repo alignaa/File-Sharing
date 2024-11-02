fff122d785c05589cc1891d0e6005fe1a95a7128
Renz <alightmotionprem229@gmail.com>
2024-11-02 19:52:33 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -1,73 +1,104 @@
-from fsub.config import FORCE_SUB_, BUTTON_ROW
+from fsub.config import FORCE_SUB_1, FORCE_SUB_2
 from pyrogram.types import InlineKeyboardButton
-from pyrogram import enums
 
-async def start_button(client):
-    if not FORCE_SUB_:
+ def start_button(client):
+    if not FORCE_SUB_1 and not FORCE_SUB_2:
         buttons = [
             [
-                InlineKeyboardButton(text="ʙᴀɴᴛᴜᴀɴ", callback_data="help"),
-                InlineKeyboardButton(text="Tutup", callback_data="close"),
+                InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+                InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
             ],
         ]
         return buttons
+    if not FORCE_SUB_1 and FORCE_SUB_2:
+        buttons = [
+            [
+                InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink2),
+            ],
+            [
+                InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+                InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
+            ],
+        ]
+        return buttons
+    if FORCE_SUB_1 and not FORCE_SUB_2:
+        buttons = [
+            [
+                InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
+            ],
+            [
+                InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+                InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
+            ],
+        ]
+        return buttons
+    if FORCE_SUB_1 and FORCE_SUB_2:
+        buttons = [
+            [
+                InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+            ],
+            [
+                InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
+                InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink2),
+            ],
+            [InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close")],
+        ]
+        return buttons
 
-    dynamic_button = []
-    current_row = []
-    for key in FORCE_SUB_.keys():
-        chat_id = FORCE_SUB_[key]
-        chat = await client.get_chat(chat_id)
-        chat_type = (
-            "ɢʀᴏᴜᴘ"
-            if chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]
-            else "ᴄʜᴀɴɴᴇʟꜱ"
-        )
-        name = chat_type
-        current_row.append(InlineKeyboardButton(text=name, url=getattr(client, f'invitelink{key}')))
-        if len(current_row) == BUTTON_ROW:
-            dynamic_button.append(current_row)
-            current_row = []
 
-    if current_row:
-        dynamic_button.append(current_row)
-
-    buttons = [
-        [
-            InlineKeyboardButton(text="ʙᴀɴᴛᴜᴀɴ", callback_data="help"),
-        ],
-    ] + dynamic_button + [
-        [InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close")],
-    ]
-    return buttons
-
-async def fsub_button(client, message):
-    if FORCE_SUB_:
-        dynamic_button = []
-        current_row = []
-        for key in FORCE_SUB_.keys():
-            chat_id = FORCE_SUB_[key]
-            chat = await client.get_chat(chat_id)
-            chat_type = (
-                "ɢʀᴏᴜᴘ"
-                if chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]
-                else "ᴄʜᴀɴɴᴇʟꜱ"
-                )
-            name = chat_type
-            current_row.append(InlineKeyboardButton(text=name, url=getattr(client, f'invitelink{key}')))
-            if len(current_row) == BUTTON_ROW:
-                dynamic_button.append(current_row)
-                current_row = []
-
-        if current_row:
-            dynamic_button.append(current_row)
-            
+def fsub_button(client, message):
+    if not FORCE_SUB_1 and FORCE_SUB_2:
+        buttons = [
+            [
+                InlineKeyboardButton(text="ᴊᴏɪɴ ɢʀᴏᴜᴘ", url=client.invitelink2),
+            ],
+        ]
         try:
-            dynamic_button.append(
+            buttons.append(
                 [
-                    InlineKeyboardButton(text="ᴄᴏʙᴀ ʟᴀɢɪ", url=f"https://t.me/{client.username}?start={message.command[1]}"),
+                    InlineKeyboardButton(
+                        text="ᴄᴏʙᴀ ʟᴀɢɪ",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
                 ]
             )
         except IndexError:
             pass
-
-        return dynamic_button
+        return buttons
+    if FORCE_SUB_1 and not FORCE_SUB_2:
+        buttons = [
+            [
+                InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="ᴄᴏʙᴀ ʟᴀɢɪ",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if FORCE_SUB_1 and FORCE_SUB_2:
+        buttons = [
+            [
+                InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
+                InlineKeyboardButton(text="ᴊᴏɪɴ ɢʀᴏᴜᴘ", url=client.invitelink2),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="ᴄᴏʙᴀ ʟᴀɢɪ",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
